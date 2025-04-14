90b4b9ecda1c6a28449da60b79c53fdafbae3087
audrbsdl <kmgni@naver.com>
2025-04-15 00:22:25 +0900

    feat: App 컴포넌트에 Input 및 TaskList 추가, 데이터 저장 및 불러오기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Input from './components/Input';
+import TaskList from './components/TaskList';
+import { getTasksFromLocalStorage, saveTasksToLocalStorage } from './storage';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = getTasksFromLocalStorage();
+    setTasks(storedTasks);
+  }, []);
+
+  const addTask = (task) => {
+    const newTasks = [...tasks, task];
+    setTasks(newTasks);
+    saveTasksToLocalStorage(newTasks);
+  }
+
   return (
     <div className="App">
       <div className="App-header">What To Do?🤔</div>
-      {/* input */}
-      {/* task list */}
+      <Input onAddTask={addTask} />
+      <TaskList tasks={tasks} />
     </div>
   );
-}
-
-function Task() {
-
-}
-
-function Input() {
-
 }
 
 export default App;
